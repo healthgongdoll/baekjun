69979db8bf49cf3f127b5b779d28592bf343e900
Jay Chung <wayne961025@gmail.com>
2021-09-15 10:51:59 -0400

    baekjun

---
+++ main_15650.java
@@ -0,0 +1,44 @@
+package eecs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class main_15650 {
+
+	public static int n, m;
+	public static int[] answer;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException{
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		answer = new int[m];
+		backtracking(0,0);
+		
+		bw.flush();
+		bw.close();
+	}
+
+	public static void backtracking(int curr, int prev)throws IOException {
+		if (curr == m) {
+			for (int i = 0; i < m; i++) {
+				bw.write(answer[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+
+		for (int i = prev + 1; i <= n; i++) {
+			answer[curr] = i;
+			backtracking(curr + 1, i);
+		}
+	}
+}
---
+++ main_15651.java
@@ -0,0 +1,56 @@
+package eecs;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class main_15651 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static int n,m;
+	public static StringTokenizer st;
+	public static int[] answer;
+	public static boolean [] visited;
+	public static void main(String []args) throws IOException
+	{
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[n+1];
+		answer = new int[m];
+		
+		backtracking(0);
+		
+		bw.flush();
+		bw.close();
+	}
+	public static void backtracking(int curr) throws IOException
+	{
+		if(curr == m)
+		{
+			for(int i = 0; i<m; i++)
+			{
+				bw.write(answer[i] + " ");
+			
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = 1; i<=n; i++)
+		{
+			
+		
+			answer[curr] = i;
+			backtracking(curr+1); // ¿Ö ++ ÀÌ¸é index out of bound?? 
+	
+		}
+		
+	}
+	
+	
+}
